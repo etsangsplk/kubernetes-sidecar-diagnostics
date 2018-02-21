a16d6b262ab8384de370a60bbfd6568c83e77180
yantang <yantang@microsoft.com>
2018-02-21 09:24:03 -0800

    Add app insights custom channel to forward data to fluentd http input

---
+++ WebFrontEnd/WebFrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Mvc;
+using WebFrontEnd.Models;
+
+namespace WebFrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(TelemetryClient client)
+        {
+            client.TrackTrace("I'm a trace...");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebFrontEnd/WebFrontEnd/FluentdHttpChannel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFrontEnd
+{
+    public class FluentdHttpChannel : ITelemetryChannel
+    {
+        private HttpClient client;
+        private Uri requestUri;
+
+        public FluentdHttpChannel(string endpoint = "http://localhost:8887")
+        {
+            this.client = new HttpClient();
+            this.EndpointAddress = endpoint;
+
+            // append channel name as tag
+            this.requestUri = new Uri(new Uri(this.EndpointAddress), "FlunetdHttpChannel");
+        }
+
+        public bool? DeveloperMode { get; set; }
+        public string EndpointAddress { get; set; }
+
+        public void Dispose()
+        {
+            this.client.Dispose();
+        }
+
+        public void Flush()
+        {
+        }
+
+        public void Send(ITelemetry item)
+        {
+            var buffer = JsonSerializer.Serialize(new []{ item }, compress: false);
+            var content = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+            client.PostAsync(this.requestUri, new StringContent(content, Encoding.UTF8, "application/json"));
+        }
+    }
+}
